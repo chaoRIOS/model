14e4b4b7d104219c442c4b3b99d60a4e98ada8d9
chaoRIOS <chao.w@rioslab.org>
2021-09-22 21:36:18 +0800

    fix(config): fix sext_word_type
    Signed-off-by: chaoRIOS <chao.w@rioslab.org>

--- config/data_types.py
+++ config/data_types.py
@@ -14,6 +14,6 @@ signed_word_type = np.int32
 def sext_word_type(data):
     return word_type(data) | (
         double_type(0xFFFF_FFFF_0000_0000)
-        if data & word_type(0x8000_0000) == 0x8000_0000
+        if word_type(data) & word_type(0x8000_0000) == word_type(0x8000_0000)
         else double_type(0)
     )
